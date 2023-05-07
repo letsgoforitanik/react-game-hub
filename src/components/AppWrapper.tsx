a444c580dbaaeee494bb13e461544cb2d8de8e06
Aniruddha Banerjee <letsgoforitanik@gmail.com>
2023-05-08 00:32:10 +0530

    Implement dark mode

--- src/components/AppWrapper.tsx
+++ src/components/AppWrapper.tsx
@@ -1,9 +1,11 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { App } from ".";
+import theme from "../theme";
 
 export default function AppWrapper() {
     return (
-        <ChakraProvider>
+        <ChakraProvider theme={theme}>
+            <ColorModeScript initialColorMode={theme.config.initialColorMode} />
             <App />
         </ChakraProvider>
     );
