4a8c06fdfab37f033ed479544693eb2d3a489d9c
Aniruddha Banerjee <letsgoforitanik@gmail.com>
2023-05-08 04:22:21 +0530

    Add .prettierrc

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,40 +1,9 @@
-import { useEffect, useState, Fragment } from "react";
+import { Fragment } from "react";
 import { Text } from "@chakra-ui/react";
-import apiClient from "../services/api-client";
-
-interface Game {
-    id: number;
-    slug: string;
-    name: string;
-    tba: boolean;
-    rating: number;
-    background_image: string;
-    rating_top: number;
-    playtime: number;
-    added: number;
-    metacritic: number;
-    suggestions_count: number;
-    reviews_text_count: string;
-}
-
-interface FetchGameResponse {
-    count: number;
-    next: string | null;
-    previous: string | null;
-    results: Game[];
-}
+import useGames from "../hooks/useGames";
 
 export default function GameGrid() {
-    const [games, setGames] = useState<Game[]>([]);
-    const [error, setError] = useState("");
-
-    useEffect(() => (fetchGames() as any) && undefined, []);
-
-    async function fetchGames() {
-        const promise = apiClient.get<FetchGameResponse>("/games");
-        promise.then((response) => setGames(response.data.results));
-        promise.catch((error) => setError(error.message));
-    }
+    const { games, error } = useGames();
 
     return (
         <Fragment>
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+import { AxiosError } from "axios";
+import apiClient from "../services/api-client";
+
+interface Game {
+    id: number;
+    slug: string;
+    name: string;
+    tba: boolean;
+    rating: number;
+    background_image: string;
+    rating_top: number;
+    playtime: number;
+    added: number;
+    metacritic: number;
+    suggestions_count: number;
+    reviews_text_count: string;
+    ratings_count: string;
+}
+
+interface FetchGameResponse {
+    count: number;
+    next: string | null;
+    previous: string | null;
+    results: Game[];
+}
+
+export default function useGames() {
+    const [games, setGames] = useState<Game[]>([]);
+    const [error, setError] = useState("");
+
+    function fetchGames() {
+        const controller = new AbortController();
+        const signal = controller.signal;
+        const promise = apiClient.get<FetchGameResponse>("/gamess", { signal });
+        promise.then((response) => setGames(response.data.results));
+        promise.catch((error) => error instanceof AxiosError && setError(error.message));
+        return () => controller.abort();
+    }
+
+    useEffect(fetchGames, []);
+
+    return { games, error };
+}
