a6ac4509c37eabd18f87c169cf5c05dbe1c5f93b
Aniruddha Banerjee <letsgoforitanik@gmail.com>
2023-05-08 17:20:36 +0530

    Show loading skeletons to genre list

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,9 +1,24 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Skeleton, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import { getCroppedImageUrl } from "../services/image-url";
 
 export default function GenreList() {
     const { genres, error, loading } = useGenres();
+    const skeletons = new Array(20).fill(0);
+
+    if (error) return null;
+
+    if (loading) {
+        return (
+            <List>
+                {skeletons.map((_, index) => (
+                    <ListItem key={index} paddingY="5px">
+                        <Skeleton height="32px" width="100%" borderRadius={8} />
+                    </ListItem>
+                ))}
+            </List>
+        );
+    }
 
     return (
         <List>
