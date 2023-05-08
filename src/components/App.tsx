5ae7307e686322dc0fabaa4eab46fa62e7e1e5f9
Aniruddha Banerjee <letsgoforitanik@gmail.com>
2023-05-08 16:53:44 +0530

    Display genres

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,13 +7,18 @@ export default function App() {
         lg: `"nav nav" "aside main"`,
     };
 
+    const templateColumns = {
+        base: "1fr",
+        lg: "200px 1fr",
+    };
+
     return (
-        <Grid templateAreas={templateAreas}>
+        <Grid templateAreas={templateAreas} templateColumns={templateColumns}>
             <GridItem area="nav">
                 <Navbar />
             </GridItem>
             <Show above="lg">
-                <GridItem area="aside">
+                <GridItem area="aside" paddingX={5}>
                     <GenreList />
                 </GridItem>
             </Show>
