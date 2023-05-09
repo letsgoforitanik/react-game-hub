882487c91716e723b4b8b7d86066437c2c7ab821
Aniruddha Banerjee <letsgoforitanik@gmail.com>
2023-05-09 12:57:08 +0530

    Change page title

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,4 +1,3 @@
-import { Fragment } from "react";
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import { GameCard, GameCardSkeleton } from ".";
 import useGames from "../hooks/useGames";
@@ -12,13 +11,12 @@ export default function GameGrid({ gameQuery }: Props) {
     const { games, error, loading } = useGames(gameQuery);
     const skeletons = new Array(15).fill(0);
 
+    if (error) return <Text>{error}</Text>;
+
     return (
-        <Fragment>
-            {error && <Text>{error}</Text>}
-            <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={10} padding={8}>
-                {loading && skeletons.map((_, index) => <GameCardSkeleton key={index} />)}
-                {!loading && games.map((game) => <GameCard key={game.id} game={game} />)}
-            </SimpleGrid>
-        </Fragment>
+        <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={10} padding={8}>
+            {loading && skeletons.map((_, index) => <GameCardSkeleton key={index} />)}
+            {!loading && games.map((game) => <GameCard key={game.id} game={game} />)}
+        </SimpleGrid>
     );
 }
